e6f9b3d7329c494894a5697848ecc68cb9edc93c
AndPolec <xardas345@gmail.com>
2023-02-18 22:20:04 +0100

    Add models

---
+++ LibraryMVC.Domain/Model/BorrowCart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMVC.Domain.Model
+{
+    public class BorrowCart
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
---
+++ LibraryMVC.Domain/Model/CheckOutRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMVC.Domain.Model
+{
+    public class CheckOutRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int LoanId { get; set; }
+        public Loan Loan { get; set; }
+        public int? LibrarianId { get; set; }
+        public Librarian Librarian { get; set; }
+    }
+}
