3668fa2ab7b15bc5dfa0911b93bb5635c35c8ca2
AndPolec <xardas345@gmail.com>
2023-02-23 22:13:47 +0100

    Add validation for AddressDetailsVm

--- LibraryMVC.Application/ViewModels/LibraryUser/AddressDetailsVm.cs
+++ LibraryMVC.Application/ViewModels/LibraryUser/AddressDetailsVm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,5 +15,18 @@ namespace LibraryMVC.Application.ViewModels.User
         public string FlatNumber { get; set; }
         public string ZipCode { get; set; }
         public string City { get; set; }
+    }
+
+    public class AddressDetailsVmValidator : AbstractValidator<AddressDetailsVm>
+    {
+        public AddressDetailsVmValidator()
+        {
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.Street).NotEmpty().Matches(@"^[A-Z].*$").WithMessage("Nazwa ulicy musi zaczynać się od dużej litery."); ;
+            RuleFor(x => x.BuildingNumber).NotEmpty();
+            RuleFor(x => x.FlatNumber).NotEmpty();
+            RuleFor(x => x.ZipCode).NotEmpty().Matches(@"\d{2}-\d{3}").WithMessage("Kod pocztowy musi mieć format: XX-XXX");
+            RuleFor(x => x.City).NotEmpty().Matches(@"^[A-Z].*$").WithMessage("Nazwa miasta musi zaczynać się od dużej litery.");
+        }
     }
 }
