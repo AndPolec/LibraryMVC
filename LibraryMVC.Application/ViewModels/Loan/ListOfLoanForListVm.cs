12083bd68070d98cb8e27d0d50ff9f1b13c97431
AndPolec <xardas345@gmail.com>
2023-03-08 22:25:40 +0100

    Added ListOfLoanForListVm

---
+++ LibraryMVC.Application/ViewModels/Loan/ListOfLoanForListVm.cs
@@ -0,0 +1,18 @@
+﻿using LibraryMVC.Application.ViewModels.Book;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMVC.Application.ViewModels.Loan
+{
+    public class ListOfLoanForListVm
+    {
+        public List<LoanForListVm> Loans { get; set; }
+        public int UserId { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int Count { get; set; }
+    }
+}
