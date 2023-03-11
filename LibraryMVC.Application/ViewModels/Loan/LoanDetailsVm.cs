b93f479055000968d0ef247bc041edde707c7064
AndPolec <xardas345@gmail.com>
2023-03-11 21:46:11 +0100

    Added LoanDetailsVm

---
+++ LibraryMVC.Application/ViewModels/Loan/LoanDetailsVm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMVC.Application.ViewModels.Loan
+{
+    public class LoanDetailsVm
+    {
+        public int Id { get; set; }
+
+    }
+}
