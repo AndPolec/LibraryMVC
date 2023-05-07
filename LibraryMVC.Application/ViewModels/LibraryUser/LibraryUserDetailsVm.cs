33b8988d03d586728e16e58ffe9a420143e20189
AndPolec <xardas345@gmail.com>
2023-05-07 21:32:25 +0200

    Added LibraryUserDetailsVm

---
+++ LibraryMVC.Application/ViewModels/LibraryUser/LibraryUserDetailsVm.cs
@@ -0,0 +1,28 @@
+﻿using LibraryMVC.Application.ViewModels.User;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMVC.Application.ViewModels.LibraryUser
+{
+    public class LibraryUserDetailsVm
+    {
+        public int Id { get; set; }
+        [DisplayName("Imię")]
+        public string FirstName { get; set; }
+        [DisplayName("Nazwisko")]
+        public string LastName { get; set; }
+        [DisplayName("E-mail")]
+        public string Email { get; set; }
+        [DisplayName("Numer telefonu")]
+        public string PhoneNumber { get; set; }
+        public AddressDetailsVm Address { get; set; }
+        [DisplayName("Suma zaległości")]
+        public decimal UnpaidPenaltiesTotal { get; set; }
+        [DisplayName("Liczba zaległych zamówień")]
+        public int OverdueLoansCount { get; set; }
+    }
+}
