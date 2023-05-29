32de6b502a0ac274318c1c6c1206afdd3a452918
AndPolec <xardas345@gmail.com>
2023-05-29 23:10:55 +0200

    Update in ConfirmReturn view

--- LibraryMVC.Application/ViewModels/Loan/LoanForConfirmReturnListVm.cs
+++ LibraryMVC.Application/ViewModels/Loan/LoanForConfirmReturnListVm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,25 @@ namespace LibraryMVC.Application.ViewModels.Loan
 {
     public class LoanForConfirmReturnListVm
     {
+        [DisplayName("Nr zamówienia")]
         public int Id { get; set; }
+
+        [DisplayName("Czytelnik")]
         public string UserName { get; set; }
+
+        [DisplayName("Data utworzenia")]
         public DateTime CreationDate { get; set; }
+
+        [DisplayName("Termin zwrotu")]
         public DateTime ReturnDueDate { get; set; }
+
+        [DisplayName("Ilość wypożyczonych książek")]
         public int NumberOfBorrowedBooks { get; set; }
+
+        [DisplayName("Naliczona kara")]
         public decimal Penalty { get; set; }
+
+        [DisplayName("Status")]
         public string Status { get; set; }
     }
 }
