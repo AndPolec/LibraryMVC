9c6d406b3a0a4e7620ccc1b55bca740c41664a34
AndPolec <xardas345@gmail.com>
2023-06-04 21:49:19 +0200

    Update in NewReturnRecordVm

--- LibraryMVC.Application/ViewModels/ReturnRecord/NewReturnRecordVm.cs
+++ LibraryMVC.Application/ViewModels/ReturnRecord/NewReturnRecordVm.cs
@@ -8,6 +8,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using FluentValidation;
+using System.ComponentModel;
 
 namespace LibraryMVC.Application.ViewModels.ReturnRecord
 {
@@ -15,13 +16,29 @@ namespace LibraryMVC.Application.ViewModels.ReturnRecord
     {
         public int Id { get; set; }
         public int LoanId { get; set; }
+
+        [DisplayName("Użytkownik")]
         public string FullLibraryUserName { get; set; }
+
+        [DisplayName("Data odebrania")]
         public DateTime CheckOutDate { get; set; }
+
+        [DisplayName("Termin zwrotu")]
         public DateTime ReturnDueDate { get; set; }
+
+        [DisplayName("Kara została zapłacona")]
         public bool IsPenaltyPaid { get; set; }
+
+        [DisplayName("Kara za przetrzymanie książek")]
         public decimal OverduePenalty { get; set; }
+
+        [DisplayName("Kara za zgubione/uszkodzone książki")]
         public decimal AdditionalPenaltyForLostAndDestroyedBooks { get; set; }
+
+        [DisplayName("Kara do zapłaty")]
         public decimal TotalPenalty { get; set; }
+
+        [DisplayName("Komentarz")]
         public string Comments { get; set; }
         public int NumberOfBorrowedBooks { get; set; }
         public List<int> LostOrDestroyedBooksId { get; set; }
