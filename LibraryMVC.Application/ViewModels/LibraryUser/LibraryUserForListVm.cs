afbc61871450565e43bc1d15e9889bcb7adb43e8
AndPolec <xardas345@gmail.com>
2023-06-08 22:35:02 +0200

    Update in LibraryUserForListVm

--- LibraryMVC.Application/ViewModels/LibraryUser/LibraryUserForListVm.cs
+++ LibraryMVC.Application/ViewModels/LibraryUser/LibraryUserForListVm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,19 @@ namespace LibraryMVC.Application.ViewModels.LibraryUser
 {
     public class LibraryUserForListVm
     {
+        [DisplayName("Nr użytkownika")]
         public int Id { get; set; }
+
+        [DisplayName("Imię i nazwisko")]
         public string FullName { get; set; }
+
+        [DisplayName("Adres email")]
         public string Email { get; set; }
+
+        [DisplayName("Suma zaległości")]
         public decimal UnpaidPenaltiesTotal { get; set; }
+
+        [DisplayName("Ilość zaległych zamówień")]
         public int OverdueLoansCount { get; set; }
     }
 }
