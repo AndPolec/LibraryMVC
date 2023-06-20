3ceca8470294c3ba78277199eba85d5246f198fa
AndPolec <xardas345@gmail.com>
2023-06-20 18:16:41 +0200

    New migration

---
+++ LibraryMVC.Infrastructure/Migrations/20230605181801_Added IsReturned field in ReturnRecord.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryMVC.Infrastructure.Migrations
+{
+    public partial class AddedIsReturnedfieldinReturnRecord : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsReturned",
+                table: "ReturnRecords",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsReturned",
+                table: "ReturnRecords");
+        }
+    }
+}
