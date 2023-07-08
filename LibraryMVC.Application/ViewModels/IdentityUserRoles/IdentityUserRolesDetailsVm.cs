2381cc168ea69c2ab2e85b8c0afcf4b3212735ff
AndPolec <xardas345@gmail.com>
2023-07-08 20:38:33 +0200

    update in _AddRolesToUser

--- LibraryMVC.Application/ViewModels/IdentityUserRoles/IdentityUserRolesDetailsVm.cs
+++ LibraryMVC.Application/ViewModels/IdentityUserRoles/IdentityUserRolesDetailsVm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,16 @@ namespace LibraryMVC.Application.ViewModels.IdentityUserRoles
 {
     public class IdentityUserRolesDetailsVm
     {
+        [DisplayName("Id")]
         public string Id { get; set; }
+
+        [DisplayName("Użytkownik")]
         public string UserName { get; set; }
+
+        [DisplayName("Uprawnienia")]
         public List<string> UserRoles { get; set; }
+
+        [DisplayName("Wybierz nowe:")]
         public List<RoleVm> AllRoles { get; set; }
     }
 }
