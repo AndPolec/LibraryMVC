0c27c407407af60dc5f821b911f164d9bf0c103c
AndPolec <xardas345@gmail.com>
2023-07-29 14:44:20 +0200

    Added Login controller

---
+++ LibraryApi/Controllers/LoginController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace LibraryApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IConfiguration _config;
+        public LoginController(SignInManager<IdentityUser> signInManager, IConfiguration config)
+        {
+            _signInManager = signInManager;
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserModel loginModel)
+        {
+            IActionResult response = Unauthorized();
+            var success = AuthenticateUser(loginModel);
+
+            if (success)
+            {
+                var tokenString = GenerateJsonWebToken(loginModel);
+                response = Ok(new { token = tokenString });
+            }
+            return response;
+        }
+
+        private bool AuthenticateUser(UserModel loginModel)
+        {
+            var result = _signInManager.PasswordSignInAsync(loginModel.Email, loginModel.Password, isPersistent: true, lockoutOnFailure: false).Result;
+            return result.Succeeded;
+        }
+
+        private object GenerateJsonWebToken(UserModel loginModel)
+        {
+            var seciurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Key"]));
+            var credentials = new SigningCredentials(seciurityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Issuer"], null, expires: DateTime.Now.AddMinutes(120), signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
