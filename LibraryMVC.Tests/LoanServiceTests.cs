a3ff09c742a8d1d039d894862e259e8789133ff5
AndPolec <xardas345@gmail.com>
2023-08-23 23:13:13 +0200

    Added LoanServiceTests

---
+++ LibraryMVC.Tests/LoanServiceTests.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using LibraryMVC.Application.Services;
+using LibraryMVC.Domain.Interfaces;
+using LibraryMVC.Domain.Model;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMVC.Tests
+{
+    public class LoanServiceTests
+    {
+        [Fact]
+        public void AddToBorrowingCart_ValidInput_ShouldUpdateBorrowingCart()
+        {
+            var mockBorrowingCartRepo = new Mock<IBorrowingCartRepository>();
+            var mockLoanRepo = new Mock<ILoanRepository>();
+            var mockBookRepo = new Mock<IBookRepository>();
+            var mockAdditionalLibrarianInfoRepo = new Mock<IAdditionalLibrarianInfoRepository>();
+            var mockReturnRecordRepo = new Mock<IReturnRecordRepository>();
+            var mockMapper = new Mock<IMapper>();
+
+            var testBorrowingCart = new BorrowingCart { Books = new List<Book>() };
+            var testBook = new Book();
+
+            mockBorrowingCartRepo.Setup(r => r.GetBorrowingCartByIdentityUserId(It.IsAny<string>())).Returns(testBorrowingCart);
+            mockBookRepo.Setup(r => r.GetBookById(It.IsAny<int>())).Returns(testBook);
+
+            var service = new LoanService(mockMapper.Object,mockBorrowingCartRepo.Object, mockLoanRepo.Object, mockBookRepo.Object, mockAdditionalLibrarianInfoRepo.Object, mockReturnRecordRepo.Object);
+            
+            service.AddToBorrowingCart(1, "userId");
+
+
+            testBorrowingCart.Books.Should().Contain(testBook);
+            mockBorrowingCartRepo.Verify(r => r.UpdateBorrowingCart(testBorrowingCart), Times.Once);
+        }
+
+        [Fact]
+        public void AddToBorrowingCart_BorrowingCartNotFound_ShouldThrowKeyNotFoundException()
+        {
+            var mockBorrowingCartRepo = new Mock<IBorrowingCartRepository>();
+            var mockLoanRepo = new Mock<ILoanRepository>();
+            var mockBookRepo = new Mock<IBookRepository>();
+            var mockAdditionalLibrarianInfoRepo = new Mock<IAdditionalLibrarianInfoRepository>();
+            var mockReturnRecordRepo = new Mock<IReturnRecordRepository>();
+            var mockMapper = new Mock<IMapper>();
+            string testUserId = "id";
+
+            mockBorrowingCartRepo.Setup(r => r.GetBorrowingCartByIdentityUserId(It.IsAny<string>())).Returns((BorrowingCart)null);
+
+            var service = new LoanService(mockMapper.Object, mockBorrowingCartRepo.Object, mockLoanRepo.Object, mockBookRepo.Object, mockAdditionalLibrarianInfoRepo.Object, mockReturnRecordRepo.Object);
+
+            Action result = () => service.AddToBorrowingCart(1, testUserId);
+
+            result.Should().Throw<KeyNotFoundException>().WithMessage($"No borrowing cart found for user with ID: {testUserId}");
+        }
+
+        [Fact]
+        public void AddToBorrowingCart_BookNotFound_ShouldThrowKeyNotFoundException()
+        {
+            var mockBorrowingCartRepo = new Mock<IBorrowingCartRepository>();
+            var mockLoanRepo = new Mock<ILoanRepository>();
+            var mockBookRepo = new Mock<IBookRepository>();
+            var mockAdditionalLibrarianInfoRepo = new Mock<IAdditionalLibrarianInfoRepository>();
+            var mockReturnRecordRepo = new Mock<IReturnRecordRepository>();
+            var mockMapper = new Mock<IMapper>();
+            int testBookId = 1;
+
+            var testBorrowingCart = new BorrowingCart { Books = new List<Book>() };
+
+            mockBorrowingCartRepo.Setup(r => r.GetBorrowingCartByIdentityUserId(It.IsAny<string>())).Returns(testBorrowingCart);
+            mockBookRepo.Setup(r => r.GetBookById(testBookId)).Returns((Book)null);
+
+            var service = new LoanService(mockMapper.Object, mockBorrowingCartRepo.Object, mockLoanRepo.Object, mockBookRepo.Object, mockAdditionalLibrarianInfoRepo.Object, mockReturnRecordRepo.Object);
+
+            Action result = () => service.AddToBorrowingCart(testBookId, "userId");
+
+            result.Should().Throw<KeyNotFoundException>().WithMessage($"No book found with ID: {testBookId}");
+        }
+
+    }
+}
