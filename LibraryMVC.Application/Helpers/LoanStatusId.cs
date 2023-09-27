ca73c5f47f4f6009356ebe70e6c461960a87d7a4
AndPolec <xardas345@gmail.com>
2023-09-27 22:49:36 +0200

    Added Helpers folder

---
+++ LibraryMVC.Application/Helpers/LoanStatusId.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMVC.Application.Helpers
+{
+    public enum LoanStatusId
+    {
+        New = 1,
+        Borrowed = 2,
+        Completed = 3,
+        Overdue = 4,
+        Cancelled = 5
+    }
+}
