d32e5ead9d7b5338756f99e64bd6fb30714096ee
AndPolec <xardas345@gmail.com>
2023-10-09 22:39:00 +0200

    Added LibraryUserServiceTests

---
+++ LibraryMVC.Tests/LibraryUserServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using LibraryMVC.Domain.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMVC.Tests
+{
+    public class LibraryUserServiceTests
+    {
+        private Mock<ILibraryUserRepository> mockLibraryUserRepo;
+        private Mock<IMapper> mockMapper;
+        private Mock<IUserTypeRepository> mockUserTypeRepo;
+        private Mock<IAdditionalLibrarianInfoRepository> mockAdditionalLibrarianInfoRepo;
+        
+        public LibraryUserServiceTests()
+        {
+            SetupMocks();
+        }
+
+        private void SetupMocks()
+        {
+            mockLibraryUserRepo = new Mock<ILibraryUserRepository>();
+            mockMapper = new Mock<IMapper>();
+            mockUserTypeRepo = new Mock<IUserTypeRepository>();
+            mockAdditionalLibrarianInfoRepo = new Mock<IAdditionalLibrarianInfoRepository>();
+        }
+
+    }
+}
