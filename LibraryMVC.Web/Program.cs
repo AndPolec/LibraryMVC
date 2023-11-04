e0ff7680d8b756813ce6f5d8dce63a84b8cea48f
AndPolec <xardas345@gmail.com>
2023-11-04 11:52:00 +0100

    Added Serilog file extension to MVC

--- LibraryMVC.Web/Program.cs
+++ LibraryMVC.Web/Program.cs
@@ -9,11 +9,13 @@ using LibraryMVC.Web.Filters;
 using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Logging.AddFile("Logs/Log-{Date}.txt");
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<Context>(options =>
     options.UseSqlServer(connectionString));
@@ -51,12 +53,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseMigrationsEndPoint();
 }
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
